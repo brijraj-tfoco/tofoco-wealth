346bba5786e2c09c2d03fc0e957e18ed0a2f24cc
brijraj <brijraj.singh@neosofttech.com>
2020-07-28 11:14:42 +0530

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>TFoco Wealth</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+        <style>
+           
+        </style>
+    </head>
+    <body class="bg-black text-white font-sans">
+       <div class="header bg-black">
+            <div class="header-nav container mx-auto mt-12 mb-12">
+                <nav class="flex items-center justify-between flex-wrap">
+                    <div class="flex items-center flex-shrink-0 text-white mr-6">
+                        <img class="w-32" src="https://www.tfoco.com/wp-content/uploads/2020/05/tfoco_white_logo.png.webp" alt="">
+                    </div>
+                    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto text-white justify-end">
+                        
+                            <a href="" class="mr-8 hover:text-yellow-700 cusrsor-pointer">WELCOME</a>
+                            <a href="" class="mr-8 hover:text-yellow-700 cusrsor-pointer">WEALTH PLANNING</a>
+                            <a href="" class="mr-8 hover:text-yellow-700 cusrsor-pointer">WHO WE ARE</a>
+                            <a href="" class="mr-8 hover:text-yellow-700 cusrsor-pointer">
+العربية</a>
+
+<button class="border-2 border-solid border-yellow-700 rounded-sm text-yellow-700 leading-none p-2">
+  LET'S TALK
+</button>
+
+
+
+                    </div>
+                </nav>
+            </div>
+       </div>
+
+       <div class=" ">
+           <div class="container mx-auto text-white text-center">
+           <ul class="inline-flex text-gray-500 font-thin">
+                <li class="mr-4"><a href="">FAMILY LEGACY</a></li> |
+                <li class="ml-4 mr-4"><a href="">PERSONALIZED MASTER PLAN</a></li> |
+                <li class="ml-4 mr-4"><a href="">TRUSTED ADVISOR</a></li> |
+                <li class="ml-4"><a href="">INVESTMENT EXCELLENENCE</a></li>
+            </ul>
+           </div>
+       </div>
+
+       <div class="banner mt-12 container mx-auto">
+            <div class="container mx-auto font-thin pl-32 ">
+
+                <div class="heading mb-4 text-yellow-700">
+                    <h1 class="text-5xl">
+                    TOGETHER WE CAN<br>PRESERVE, GROW, AND<br> SECURE YOUR LEGACY
+                    </h1>
+                </div>
+
+                <p class="text-white">
+                You can rely on our shared heritage to create personalized wealth management solutions that ensure your legacy endures and multiplies through generations, preserving your lifestyle and core values.
+                </p>
+            </div>
+       </div>
+
+       <div class="contact-us container mx-auto mt-20">
+            <div class="flex bg-yellow-700 text-black font-thin p-4">
+                <div class="flex-none">
+                    <h2>
+                    YOUR FAMILY, OUR COMMITMENT
+                    </h2>
+                    <h1>
+                    TAKE THE FIRST STEP TO A BESPOKE PORTFOLIO
+
+                    </h1>
+                </div>
+
+                <div class="flex w-full justify-end">
+                <button class="bg-gray-800 text-yellow-700 p-4">
+                    LET US CONTACT YOU
+                </button>
+                </div>
+                
+            </div>
+            
+        </div>
+
+       <div class="container mx-auto align-center mt-32">
+            <div class="flex -mx-2">
+                <div class="w-1/2 px-2">
+                    <div class="bg-gray-900 p-12 font-thin relative" style="left:40px;top:20px;">
+                        <h1 class="text-4xl mb-2">LOCAL KNOWLEDGE, GLOBAL REACH</h1>
+                        <p>Individuals and families in the GCC enjoy the unique advantage of belonging to a fast-evolving globalized region. Are you worried about economic, geo-political, interest rate and other risks? Does the emergence of new sectors and asset classes make you wonder how you can diversify your investments? We help you understand and keep pace with global developments, so your wealth is shielded against risk and volatility. Our DNA ties us to the region. On a foundation of a shared heritage, we deliver international best practice tailored to local requirements.
+                        </p>
+                    </div>
+                </div>
+                <div class="w-1/2 px-2">
+                    <img src="https://d1hkv6sew914vt.cloudfront.net/wp-content/uploads/2019/10/17055043/personalApproach%402x-e1563208006858.jpg" alt="">
+                </div>
+            </div>
+                
+       </div>
+
+       <div class="container mx-auto align-center mt-32">
+            <div class="flex -mx-2">
+                <div class="w-1/2 px-2">
+                    <img src="https://d1hkv6sew914vt.cloudfront.net/wp-content/uploads/2019/10/17055403/famLegacyOverview%402x-e1563208287318.jpg" alt="">
+                    
+                </div>
+                <div class="w-1/2 px-2">
+                    <div class="bg-gray-900 p-12 font-thin relative" style="left:-60px;top:40px;">
+                        <h1 class="text-4xl mb-2">WEALTH ISN’T JUST MONEY. IT’S AN ENDURING FAMILY LEGACY</h1>
+                        <p>Your legacy is your gift to future generations. Talk to us about creating a diversified and risk-weighted investment portfolio uniquely suited for long-term wealth preservation.
+                        </p>
+                        <button class="border-2 border-solid border-yellow-700 rounded-sm text-yellow-700 leading-none p-2 mt-4">
+                            YOUR FAMILY LEGACY
+                        </button>
+                    </div>
+                </div>
+            </div>
+                
+       </div>
+
+       <div class="container mx-auto align-center mt-40">
+            <div class="flex -mx-2">
+                <div class="w-1/2 px-2">
+                    <div class="bg-gray-900 p-12 font-thin relative" style="left:60px;top:-40px;">
+                        <h1 class="text-4xl mb-2">A WEALTH PLAN TAILORED TO YOUR ASPIRATIONS</h1>
+                        <p>Our seasoned investment professionals will tailor a portfolio based on a deep understanding of your values, ambitions and aspirations, unlocking exclusive opportunities while mitigating risk.
+                        </p>
+                        <button class="border-2 border-solid border-yellow-700 rounded-sm text-yellow-700 leading-none p-2 mt-4">
+                            LET'S BUILD YOUR PLAN
+                        </button>
+                    </div>
+                    
+                </div>
+                <div class="w-1/2 px-2">
+                    <img src="https://d1hkv6sew914vt.cloudfront.net/wp-content/uploads/2019/10/17055813/perMastPlanOverview%402x-e1563208399602.jpg" alt="">
+                    
+                </div>
+            </div>
+                
+       </div>
+
+       <div class="container mx-auto align-center mt-32">
+            <div class="flex -mx-2">
+                <div class="w-1/2 px-2">
+                    <img class="w-full" src="https://d1hkv6sew914vt.cloudfront.net/wp-content/uploads/2019/10/17060453/trustedAdvisorOverview%402x-e1563208502338.jpg" alt="">
+                    
+                </div>
+                <div class="w-1/2 px-2">
+                    <div class="bg-gray-900 p-12 font-thin relative" style="top:40px;">
+                        <h1 class="text-4xl mb-2">INDEPENDENT FINANCIAL ADVICE</h1>
+                        <p>As an independent financial advisor aligned to your interests, we maintain a transparent fee structure and ensure there is no conflict of interest. With proven investment excellence and effective risk mitigation, we navigate your legacy through divergent economic cycles over the long run.</p>
+                        <button class="border-2 border-solid border-yellow-700 rounded-sm text-yellow-700 leading-none p-2 mt-8">
+                            WHY WE ARE TRUSTED
+                        </button>
+                    </div>
+                </div>
+            </div>
+                
+       </div>
+
+       <div class="container mx-auto align-center mt-40">
+            <div class="flex -mx-2">
+                <div class="w-1/2 px-2">
+                    <div class="bg-gray-900 p-12 font-thin relative" style="left:60px;top:-40px;">
+                        <h1 class="text-4xl mb-2">FINANCIAL PLANNING THAT MATCHES YOUR LIFE GOALS</h1>
+                        <p>Our five-factor investment philosophy assures optimal risk-adjusted returns through tax-efficient portfolios.
+                        </p>
+                        <button class="border-2 border-solid border-yellow-700 rounded-sm text-yellow-700 leading-none p-2 mt-8">
+                            OUR INVESTMENT PHILOSOPHY
+                        </button>
+                    </div>
+                    
+                </div>
+                <div class="w-1/2 px-2">
+                    <img src="https://d1hkv6sew914vt.cloudfront.net/wp-content/uploads/2019/10/17060458/invExcOverview%402x-e1563208617418.jpg" alt="">
+                    
+                </div>
+            </div>
+                
+       </div>
+
+       <div class="investing-insights container mx-auto align-center mt-32">
+            <div class="text-center mb-12">
+                <h2 class="text-4xl font-thin">INVESTING INSIGHTS</h2>
+                <span class=" text-lg font-thin">We keep a close watch on investment trends that matter.</span>
+            </div>
+            
+            <div class="flex -mx-2 container mx-auto">
+
+                <div class="w-1/3 px-2">
+                    <div class="max-w-sm rounded overflow-hidden shadow-lg bg-gray-900">
+                        <img class="w-full" src="https://www.tfoco.com/wp-content/uploads/2019/09/AI-5G-April-2019-1-1-1-300x200.jpg.webp" alt="">
+                        <div class="px-6 py-4">
+                            <div class="font-thin text-xl mb-2">Finding Value in US Bank Stocks</div>
+                            <p class="font-thin text-base">
+                            US Bank Stocks declined sharply in 2018
+                            </p>
+                            <p class="font-thin text-base mt-4 text-gray-400">
+                            On May 1 2019
+                            </p>
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <div class="w-1/3 px-2">
+
+                    <div class="max-w-sm rounded overflow-hidden shadow-lg bg-gray-900">
+                        <img class="w-full" src="https://www.tfoco.com/wp-content/uploads/2019/09/Dos-and-Donts-1-300x200.jpg.webp" alt="">
+                        <div class="px-6 py-4">
+                        <div class="font-thin text-xl mb-2">Global disruption ins 5G and AI</div>
+                            <p class="font-thin text-base">
+                            US Bank Stocks declined sharply in 2018
+                            </p>
+                            <p class="font-thin text-base mt-4 text-gray-400">
+                            On May 1 2019
+                            </p>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+
+                <div class="w-1/3 px-2">
+
+                    <div class="max-w-sm rounded overflow-hidden shadow-lg bg-gray-900">
+                        <img class="w-full" src="https://www.tfoco.com/wp-content/uploads/2019/09/Oil-Update-1-300x199.jpg.webp" alt="">
+                        <div class="px-6 py-4">
+                        <div class="font-thin text-xl mb-2">Do's and Don'ts</div>
+                            <p class="font-thin text-base">
+                            US Bank Stocks declined sharply in 2018
+                            </p>
+                            <p class="font-thin text-base mt-4 text-gray-400">
+                            On May 1 2019
+                            </p>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+
+              
+            </div>
+       </div>
+
+       
+
+            <!---  Footer Section -->
+            <div class="container px-2 mx-auto footer text-gray-400 mt-56">
+            <div class="flex -mx-2">
+
+                <div class="w-1/4 px-2">
+                  <img class="w-1/2" src="https://www.tfoco.com/wp-content/uploads/2020/05/tfoco_white_logo.png.webp" alt="The Family Office Logo" height="156" width="349">
+                  <ul>
+                    <li class="mt-4"><a href="">inquiry@tfoco.com</a></li>
+                    <li class="mt-4"><a href="">+9734744883</a></li>
+                    <li class="mt-4"><a href="">The Family Office</a></li>
+                  </ul>
+                </div>
+                <div class="w-1/4 px-2">
+                    <ul>
+                        <li class="mt-4"><a href="">Welcome</a></li>
+                        <li class="mt-4"><a href="">Wealth Planning</a></li>
+                        <li class="mt-4"><a href="">Who We Are</a></li>
+
+                    </ul>
+                </div>
+                <div class="w-1/4 px-2">
+                    <ul>
+                        <li class="mt-4"><a href="">Board Of Directors</a> </li>
+                        <li class="mt-4"><a href="">Terms and Conditions</a> </li>
+                        <li class="mt-4"><a href="">Data Privacy</a></li>
+                        <li class="mt-4"><a href="">Corporate Governance</a></li>
+                        <li class="mt-4"><a href="">Compliance</a></li>
+                    </ul>
+                </div>
+                <div class="w-1/4 px-2 mt--4">
+                    <img src="https://www.tfoco.com/wp-content/uploads/2019/10/app-store-img-3x-300x235.png.webp" alt="">
+                </div>
+            </div>
+            </div>
+            
+    </body>
+</html>
